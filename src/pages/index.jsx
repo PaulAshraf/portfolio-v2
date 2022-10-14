853659c2ff6b1565876fc441f92e04e4b1801102
Paul Ashraf <paulashraf14@gmail.com>
2022-10-14 04:19:33 +0200

    chore: display more articles

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -76,7 +76,7 @@ export async function getStaticProps() {
   return {
     props: {
       articles: (await getAllArticles())
-        .slice(0, 1)
+        .slice(0, 3)
         .map(({ component, ...meta }) => meta),
     },
   }
